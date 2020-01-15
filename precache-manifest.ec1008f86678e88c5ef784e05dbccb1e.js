c8753ceafc6f5048fd99d8e2adf6a6271cedacc4
Shaiful Shafian <shaifulshafian@gmail.com>
2020-01-15 13:17:48 +0800

    Updates

---
+++ precache-manifest.ec1008f86678e88c5ef784e05dbccb1e.js
@@ -0,0 +1,434 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564b26ee8398eaeae827331c15703bbe",
+    "url": "/trarut-git/index.html"
+  },
+  {
+    "revision": "6f8ae47305dbf4f23e5b",
+    "url": "/trarut-git/static/css/2.6bc7e310.chunk.css"
+  },
+  {
+    "revision": "9e49d18a4e66c1616c22",
+    "url": "/trarut-git/static/css/main.a2c86985.chunk.css"
+  },
+  {
+    "revision": "6f8ae47305dbf4f23e5b",
+    "url": "/trarut-git/static/js/2.7632f3ac.chunk.js"
+  },
+  {
+    "revision": "8b0854dbace574fdd9bdaf468e523a50",
+    "url": "/trarut-git/static/js/2.7632f3ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e49d18a4e66c1616c22",
+    "url": "/trarut-git/static/js/main.cdde59f4.chunk.js"
+  },
+  {
+    "revision": "2ee6e95d2e60c6dbedf1",
+    "url": "/trarut-git/static/js/runtime-main.6dd42b7d.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/trarut-git/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/trarut-git/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/trarut-git/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/trarut-git/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/trarut-git/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/trarut-git/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/trarut-git/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/trarut-git/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/trarut-git/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/trarut-git/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/trarut-git/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/trarut-git/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/trarut-git/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/trarut-git/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/trarut-git/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/trarut-git/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/trarut-git/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/trarut-git/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/trarut-git/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/trarut-git/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "d0558d91063038236b60e3ef71fdc1fd",
+    "url": "/trarut-git/static/media/app-store-badge.d0558d91.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/trarut-git/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/trarut-git/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "36ddbb2de498313fdacd1b2a64eb8363",
+    "url": "/trarut-git/static/media/bg-cta.36ddbb2d.jpg"
+  },
+  {
+    "revision": "82bcf3c843c811de878424e8d8307250",
+    "url": "/trarut-git/static/media/demo-screen-1.82bcf3c8.jpg"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/trarut-git/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/trarut-git/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/trarut-git/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/trarut-git/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/trarut-git/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/trarut-git/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/trarut-git/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/trarut-git/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/trarut-git/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/trarut-git/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/trarut-git/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/trarut-git/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/trarut-git/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/trarut-git/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/trarut-git/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "03b51e065c5c626999376722f5bfd180",
+    "url": "/trarut-git/static/media/galaxy_s5_land_black.03b51e06.png"
+  },
+  {
+    "revision": "0525c34cef3283eabc3167338a0797d4",
+    "url": "/trarut-git/static/media/galaxy_s5_land_white.0525c34c.png"
+  },
+  {
+    "revision": "0de8e1a8665635a0837688589c6f4914",
+    "url": "/trarut-git/static/media/galaxy_s5_port_black.0de8e1a8.png"
+  },
+  {
+    "revision": "5910135fbce4a7641a5f1ff1400b9d7c",
+    "url": "/trarut-git/static/media/galaxy_s5_port_white.5910135f.png"
+  },
+  {
+    "revision": "c10e360713bdd5a37ccc7084a4402857",
+    "url": "/trarut-git/static/media/galaxy_tab4_land_black.c10e3607.png"
+  },
+  {
+    "revision": "ba2ef5665e4e31ba27c939cc60589963",
+    "url": "/trarut-git/static/media/galaxy_tab4_land_white.ba2ef566.png"
+  },
+  {
+    "revision": "7f10264e0f100ddffd8bbdd8ac281fef",
+    "url": "/trarut-git/static/media/gold.7f10264e.png"
+  },
+  {
+    "revision": "cfea2442b66fe18c84e0f51c29486192",
+    "url": "/trarut-git/static/media/google-play-badge.cfea2442.svg"
+  },
+  {
+    "revision": "f33471e2767747a3863115d7b8b980b8",
+    "url": "/trarut-git/static/media/grey.f33471e2.png"
+  },
+  {
+    "revision": "b4111cf5c1d1896d1628f95e3bea7e48",
+    "url": "/trarut-git/static/media/imac.b4111cf5.png"
+  },
+  {
+    "revision": "3d8f03daaeaca835b53aef996630a1eb",
+    "url": "/trarut-git/static/media/ipad_air_2_gold_land.3d8f03da.png"
+  },
+  {
+    "revision": "18c7b36437e59d55e4c8c89898664b9c",
+    "url": "/trarut-git/static/media/ipad_air_2_gold_port.18c7b364.png"
+  },
+  {
+    "revision": "43cbe56f89906ad1095dcb411f54ea79",
+    "url": "/trarut-git/static/media/ipad_air_2_gray_land.43cbe56f.png"
+  },
+  {
+    "revision": "070f84dc1bf0ceb5b09710a62b0c044c",
+    "url": "/trarut-git/static/media/ipad_air_2_gray_port.070f84dc.png"
+  },
+  {
+    "revision": "eeb81c4553931271a6fcddc76aa8e2a5",
+    "url": "/trarut-git/static/media/ipad_air_2_silver_land.eeb81c45.png"
+  },
+  {
+    "revision": "cb6895ef16c1ce0b4f4d9d0ee1e3f496",
+    "url": "/trarut-git/static/media/ipad_air_2_silver_port.cb6895ef.png"
+  },
+  {
+    "revision": "49d7d7738efffe81e5465076ed255f29",
+    "url": "/trarut-git/static/media/ipad_air_gray_land.49d7d773.png"
+  },
+  {
+    "revision": "3aab1adee35d0ad688992735a96a52c2",
+    "url": "/trarut-git/static/media/ipad_air_gray_port.3aab1ade.png"
+  },
+  {
+    "revision": "b7cbef7b31f10fb0ec94894ade316ee1",
+    "url": "/trarut-git/static/media/ipad_air_silver_land.b7cbef7b.png"
+  },
+  {
+    "revision": "cc19f51ea28d907e545a6f57ecaa3830",
+    "url": "/trarut-git/static/media/ipad_air_silver_port.cc19f51e.png"
+  },
+  {
+    "revision": "76c5ba8820448f36ee9567acbf7e5ee5",
+    "url": "/trarut-git/static/media/ipad_port_black.76c5ba88.png"
+  },
+  {
+    "revision": "94c538da19ff004085dfc3093a88e748",
+    "url": "/trarut-git/static/media/ipad_pro_land_black.94c538da.png"
+  },
+  {
+    "revision": "b3faae9e1082811badead6cfd98db62f",
+    "url": "/trarut-git/static/media/ipad_pro_land_gold.b3faae9e.png"
+  },
+  {
+    "revision": "9fb9a4146751e9b044ce14dad13588c9",
+    "url": "/trarut-git/static/media/ipad_pro_land_silver.9fb9a414.png"
+  },
+  {
+    "revision": "b6239c60fcca3b51b0f3b2d8e11f1af9",
+    "url": "/trarut-git/static/media/ipad_pro_port_black.b6239c60.png"
+  },
+  {
+    "revision": "f0e7b1cf8ef8f58d07f11e5e96c4a03a",
+    "url": "/trarut-git/static/media/ipad_pro_port_gold.f0e7b1cf.png"
+  },
+  {
+    "revision": "c1ffcacd23f696e90b004e4a0c54349b",
+    "url": "/trarut-git/static/media/ipad_pro_port_silver.c1ffcacd.png"
+  },
+  {
+    "revision": "07bbd15ff3c2a1859feb82afb719e2cf",
+    "url": "/trarut-git/static/media/iphone5_land_black.07bbd15f.png"
+  },
+  {
+    "revision": "5d4198bc85fcb3c350950f0a69ec9907",
+    "url": "/trarut-git/static/media/iphone5_land_white.5d4198bc.png"
+  },
+  {
+    "revision": "81078e2cf3b4fe341431f4d2f6675eed",
+    "url": "/trarut-git/static/media/iphone5_port_black.81078e2c.png"
+  },
+  {
+    "revision": "3347526fc4a1fdedcabe680c64c0e799",
+    "url": "/trarut-git/static/media/iphone5_port_white.3347526f.png"
+  },
+  {
+    "revision": "6ca71745c1ce3e4b0ca33740afe159fa",
+    "url": "/trarut-git/static/media/iphone_6_land_black.6ca71745.png"
+  },
+  {
+    "revision": "633e2b154c400e9a9cfcc568ddaafdb0",
+    "url": "/trarut-git/static/media/iphone_6_land_gold.633e2b15.png"
+  },
+  {
+    "revision": "aa01efc5702705ce5a695da66aed8faf",
+    "url": "/trarut-git/static/media/iphone_6_land_white.aa01efc5.png"
+  },
+  {
+    "revision": "ae1f5e7ab3d5a28ac3a858de6764f7df",
+    "url": "/trarut-git/static/media/iphone_6_plus_black_land.ae1f5e7a.png"
+  },
+  {
+    "revision": "d96cfb83c4d774edd86e0e06b47a974b",
+    "url": "/trarut-git/static/media/iphone_6_plus_black_port.d96cfb83.png"
+  },
+  {
+    "revision": "b8846e1c45561164e04a43abc5a5c75a",
+    "url": "/trarut-git/static/media/iphone_6_plus_gold_land.b8846e1c.png"
+  },
+  {
+    "revision": "0aec69baa3d9026b3dd7dd88e054f35e",
+    "url": "/trarut-git/static/media/iphone_6_plus_gold_port.0aec69ba.png"
+  },
+  {
+    "revision": "3b0218f7b660c875ec8440fd9279df00",
+    "url": "/trarut-git/static/media/iphone_6_plus_white_land.3b0218f7.png"
+  },
+  {
+    "revision": "d6c50fbd2ce470c075e863ba6961557a",
+    "url": "/trarut-git/static/media/iphone_6_plus_white_port.d6c50fbd.png"
+  },
+  {
+    "revision": "ae61d78586a588605a71f0f58f9e2760",
+    "url": "/trarut-git/static/media/iphone_6_port_black.ae61d785.png"
+  },
+  {
+    "revision": "673d810a0ec7218572346bf8bd3b8d90",
+    "url": "/trarut-git/static/media/iphone_6_port_gold.673d810a.png"
+  },
+  {
+    "revision": "d4956eac0ce11c5df72ab79fb58815b9",
+    "url": "/trarut-git/static/media/iphone_6_port_white.d4956eac.png"
+  },
+  {
+    "revision": "f17bc857d0dcea6fe60775161c7bbd5e",
+    "url": "/trarut-git/static/media/iphone_se_land_black.f17bc857.png"
+  },
+  {
+    "revision": "8d33a7c1f6305b41c56523d5bf7e2176",
+    "url": "/trarut-git/static/media/iphone_se_land_gold.8d33a7c1.png"
+  },
+  {
+    "revision": "1c34c98f53b119c958988496a60a03c3",
+    "url": "/trarut-git/static/media/iphone_se_land_rose.1c34c98f.png"
+  },
+  {
+    "revision": "6c71f01ff2b3a48c38a70a43ef9b3914",
+    "url": "/trarut-git/static/media/iphone_se_land_white.6c71f01f.png"
+  },
+  {
+    "revision": "e392031a42566e89724cc347e2eb944e",
+    "url": "/trarut-git/static/media/iphone_se_port_black.e392031a.png"
+  },
+  {
+    "revision": "b49805dee4cc2d7b8c92d3632ba8bcb9",
+    "url": "/trarut-git/static/media/iphone_se_port_gold.b49805de.png"
+  },
+  {
+    "revision": "664550ad8bff9d496e4786641e3033d5",
+    "url": "/trarut-git/static/media/iphone_se_port_rose.664550ad.png"
+  },
+  {
+    "revision": "f947505c6fa2adb20690a2474736ab0a",
+    "url": "/trarut-git/static/media/iphone_se_port_white.f947505c.png"
+  },
+  {
+    "revision": "cd3a76c6e24267e669f6bc5ad2ff9f6e",
+    "url": "/trarut-git/static/media/lumia920_land.cd3a76c6.png"
+  },
+  {
+    "revision": "accc02ae3e69c7a2679d43b1979811a7",
+    "url": "/trarut-git/static/media/lumia920_port.accc02ae.png"
+  },
+  {
+    "revision": "e2d887e445fd459e71304f3caae7610f",
+    "url": "/trarut-git/static/media/macbook.e2d887e4.png"
+  },
+  {
+    "revision": "20bfe0b646677b54b0d7af8921aee885",
+    "url": "/trarut-git/static/media/nexus7_port.20bfe0b6.png"
+  },
+  {
+    "revision": "6b2944e5a402051f87606434560c7b38",
+    "url": "/trarut-git/static/media/s3_land_black.6b2944e5.png"
+  },
+  {
+    "revision": "0effbb6a3ffa421ab3b6da2e69fb3b0e",
+    "url": "/trarut-git/static/media/s3_land_white.0effbb6a.png"
+  },
+  {
+    "revision": "fda6e17702dbe65c69ca219d9d2bb06a",
+    "url": "/trarut-git/static/media/s3_port_black.fda6e177.png"
+  },
+  {
+    "revision": "54ef3f9543f5e6134bee56473edec3a4",
+    "url": "/trarut-git/static/media/s3_port_white.54ef3f95.png"
+  },
+  {
+    "revision": "4197172869623c61e72b07ff19e98c2f",
+    "url": "/trarut-git/static/media/samsung_tv.41971728.png"
+  },
+  {
+    "revision": "845604f7ac5c5d44155d61484944ca07",
+    "url": "/trarut-git/static/media/silver.845604f7.png"
+  }
+]);
